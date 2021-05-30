d7a6af53f40270237dd77a4e3d4b3c59e64bbe5f
GitHub Action <action@github.com>
2021-05-30 20:36:06 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/snowywhitee/timemachineplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.snowywhitee.timemachineplugin.services
+
+import com.github.snowywhitee.timemachineplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/snowywhitee/timemachineplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/snowywhitee/timemachineplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.snowywhitee.timemachineplugin.services
 
+import com.github.snowywhitee.timemachineplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
